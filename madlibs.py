377ecb392248d302f60351de1876469b64f22d94
Tamharpreet <32073128+Tamharpreet@users.noreply.github.com>
2021-04-12 19:20:41 -0700

    Add files via upload

---
+++ madlibs.py
@@ -0,0 +1,23 @@
+def madlib():
+    bodypart= input("Body Part:")
+    verb = input("Verb: ")
+    adj1 = input("Adjective: ")
+    adj2 = input("Adjective: ")
+    adj3 = input("Adjective: ")
+    noun1 = input("Noun: ")
+    noun2 = input("Noun: ")
+
+
+    madlib = f"I love computer programming because it's {adj1}! The journey to become good programmer \
+starts with hope in your mind and dream in your  {bodypart}. People say coding is tough might be \
+{adj1} and they dont what actually lies behind the doors of {adj2}. Yes, once you learn to {verb}, \
+it becomes a part of your life identity and you can become a super {adj3} hacker. Knowledge of programming \
+lets you take control of your {noun1}."
+
+
+
+    print (madlib)
+
+madlib()
+
+
